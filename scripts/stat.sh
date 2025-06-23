99b8b93d3d252f7bd2855337feb05f9f8c5caca5
vsmirn0v <40442245+vsmirn0v@users.noreply.github.com>
2025-06-23 14:16:02 +0300

    reduce hardcode param dependency

--- scripts/stat.sh
+++ scripts/stat.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
 
+egrep_pattern=$(hostname -I | tr ' ' '\n' | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | awk '{print $0 ":443"}' | paste -sd'|' -)
+
 if [[ $(cat /tmp/vpnlog.txt |wc -l) -gt 4000 ]]; then
   tail -2000 /tmp/vpnlog.txt > /tmp/vpnlog.txt.new
   mv /tmp/vpnlog.txt.new /tmp/vpnlog.txt
 fi
 
-netstat -antplu|grep '159.223.0.95:443'|grep -v '46.39.254.20'|grep EST | while IFS= read -r line; do echo "$(TZ=Etc/GMT-3 date '+[%Y-%m-%d %H:%M:%S]') $line"; done >> /tmp/vpnlog.txt
+netstat -antplu|egrep "($egrep_pattern)"|grep -v '46.39.254.20'|grep EST | while IFS= read -r line; do echo "$(TZ=Etc/GMT-3 date '+[%Y-%m-%d %H:%M:%S]') $line"; done >> /tmp/vpnlog.txt
